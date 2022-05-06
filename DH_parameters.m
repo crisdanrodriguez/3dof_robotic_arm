28e9639680a99867a76f32cda2d7508470681e86
Cristian <cristian.714@hotmail.com>
2022-05-05 22:21:26 -0500

    Update

--- DH_parameters.m
+++ DH_parameters.m
@@ -5,12 +5,12 @@ function [DH, q] = DH_parameters(theta1_deg, theta2_deg, theta3_deg)
     theta3_rad = round(theta3_deg) * (pi/180);
     
     % Variables values
-    D1 = 131; L2 = 91.7; L3 = 100;
+    D1 = 131; L2 = 91.7; L3 = 100; D4 = 50;
 
     % DH parameters
-    DH.sigma = [0, 0, 0];
-    DH.a = [0, L2, L3].';
-    DH.alpha = [-pi/2, 0, 0].';
-    DH.d = [D1, 0, 0].';
-    q = [theta1_rad, theta2_rad, theta3_rad];
+    DH.sigma = [0, 0, 0, 1];
+    DH.a = [0, L2, L3, 0].';
+    DH.alpha = [-pi/2, 0, -pi/2, 0].';
+    DH.d = [D1, 0, 0, D4].';
+    q = [theta1_rad, theta2_rad, theta3_rad, 0];
 end
