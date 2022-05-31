479b3192ac52495f4d48cac9247c248a5b372e2d
Cristian <cristian.714@hotmail.com>
2022-05-31 18:19:35 -0500

    Update

--- IGM.m
+++ IGM.m
@@ -13,8 +13,19 @@ function qs = IGM(P4X, P4Y, P4Z)
     q23_a = type7(W1, W2, X, Y, Z1_a, Z2);
     q23_b = type7(W1, W2, X, Y, Z1_b, Z2);
 
-    qs = [q1(1), q23_a(1,1), q23_a(2,1);
-          q1(1), q23_a(1,2), q23_a(2,2);
-          q1(2), q23_b(1,1), q23_b(2,1);
-          q1(2), q23_b(1,2), q23_b(2,2)];
+    qs = [
+        q1(1), q23_a(1,1), q23_a(2,1);
+        q1(1), q23_a(1,2), q23_a(2,2);
+        q1(2), q23_b(1,1), q23_b(2,1);
+        q1(2), q23_b(1,2), q23_b(2,2)];
+    
+    qs_13 = [(wrapTo180(rad2deg(qs(:,1)))),(wrapTo180(rad2deg(qs(:,3))))];
+    qs_2 = wrapTo180(rad2deg(qs(:,2)));
+    for n=1:4
+        if qs_2(n) > 60
+            qs_2(n) = qs_2(n)-360;
+        end
+    end
+    qs = [qs_13(:,1) qs_2 qs_13(:,2)];
+    
 end
