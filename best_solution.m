0d7917f1b6dd94fd814846f958a07dcaec59d8c2
Cristian <cristian.714@hotmail.com>
2022-06-01 20:48:44 -0500

    Update

--- best_solution.m
+++ best_solution.m
@@ -4,10 +4,14 @@ function qs_best = best_solution(qs_good, theta1_deg, theta2_deg, theta3_deg)
 
     % Calculate the error for each set of qs
     for i = 1: n 
-        e_1 = abs(theta1_deg - qs_good(i, 1));
-        e_2 = abs(theta2_deg - qs_good(i, 2));
-        e_3 = abs(theta3_deg - qs_good(i, 3));
-        e_sum(end+1,:) = e_1 + e_2 + e_3;
+        if qs_good(i,1) ~= 300
+            e_1 = abs(theta1_deg - qs_good(i, 1));
+            e_2 = abs(theta2_deg - qs_good(i, 2));
+            e_3 = abs(theta3_deg - qs_good(i, 3));
+            e_sum(end+1,:) = e_1 + e_2 + e_3;
+        else
+            e_sum(end+1,:) = 1000;
+        end
     end
     % Get the qs with the minimum error
     [min_value, min_index] = min(e_sum);
--- possible_solutions.m
+++ possible_solutions.m
@@ -1,14 +1,16 @@
-function qs_good = possible_solutions(qs_deg)
+function [qs_good, qs_bad] = possible_solutions(qs_deg)
     n = length(qs_deg);
-    qs_good = [];
-    qs_bad = [];
+    qs_good = []
+    qs_bad = []
     
     % Classify if solution as good or bad according to the thetas values
     for i = 1: n
         if qs_deg(i,1) < -150 || qs_deg(i,1) > 150 || qs_deg(i,3) < -150 || qs_deg(i,3) > 150 || qs_deg(i,2) > 60 || qs_deg(i,2) < -240
             qs_bad(end+1,:) = qs_deg(i,:);
+            qs_good(end+1,:) = [300, 300, 300];
         else
             qs_good(end+1,:) = qs_deg(i,:);
+            qs_bad(end+1,:) = [300, 300, 300];
         end
     end
 end
