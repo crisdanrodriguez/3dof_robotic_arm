d75bd5143320d0ee47cb5879e9ecbfd502441504
Cristian <cristian.714@hotmail.com>
2022-06-07 18:37:10 -0500

    Update

--- possible_solutions.m
+++ possible_solutions.m
@@ -1,11 +1,11 @@
 function [qs_good, qs_bad] = possible_solutions(qs_deg)
-    n = length(qs_deg);
-    qs_good = []
-    qs_bad = []
+    n = height(qs_deg);
+    qs_good = [];
+    qs_bad = [];
     
     % Classify if solution as good or bad according to the thetas values
     for i = 1: n
-        if qs_deg(i,1) < -150 || qs_deg(i,1) > 150 || qs_deg(i,3) < -150 || qs_deg(i,3) > 150 || qs_deg(i,2) > 60 || qs_deg(i,2) < -240
+        if double(subs(qs_deg(i,1))) < -150 || double(subs(qs_deg(i,1))) > 150 || double(subs(qs_deg(i,3))) < -150 || double(subs(qs_deg(i,3))) > 150 || double(subs(qs_deg(i,2))) > 60 || double(subs(qs_deg(i,2))) < -200
             qs_bad(end+1,:) = qs_deg(i,:);
             qs_good(end+1,:) = [300, 300, 300];
         else
